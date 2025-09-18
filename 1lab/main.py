14da7a652430e755093eb847cd36290b0bca53e3
iamkatkov <iamkatkov@gmail.com>
2025-09-18 23:04:17 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,24 @@
+def f(seats, K):
+    N = len(seats)
+    if N == 0:
+        return (-1, -1)
+    M = len(seats[0])
+    
+    for i in range(N):
+        k = 0
+        for j in range(M):  
+            if seats[i][j] == 0:  
+                k += 1
+                if k == K:  
+                    start = j - K + 1  
+                    for pos in range(start, j + 1):  
+                        seats[i][pos] = 1
+                    return (i, start)
+            else:
+                k = 0  
+    return (-1, -1) 
 
+seats = [[1, 0, 1, 0, 1],[1, 0, 0, 1, 0],[0, 0, 0, 0, 0]]
+K = 2
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+print(f(seats, K))
